df547e2d122ca5c94a15023939b73c3d01261115
Inara <inarasalih2015@gmail.com>
2021-01-09 15:53:26 -0600

    User Story 1 added changes

--- src/test/java/batch21_group29/UserStory_1.java
+++ src/test/java/batch21_group29/UserStory_1.java
@@ -3,5 +3,6 @@ package batch21_group29;
 public class UserStory_1 {
     public static void main(String[] args) {
         System.out.println("Hello my friends...!");
+        System.out.println("Here guys I`m going to create all the steps for my user story");
     }
 }
