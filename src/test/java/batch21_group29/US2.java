0ef24b4d080373f655b2413bf37888ab62c6e8f7
Inara <inarasalih2015@gmail.com>
2021-01-09 15:41:35 -0600

    User Story 2 created

--- src/test/java/batch21_group29/US2.java
+++ src/test/java/batch21_group29/US2.java
@@ -1,3 +1,5 @@
+package batch21_group29;
+
 public class US2 {
     public static void main(String[] args) {
         System.out.println("YAY!");
---
+++ src/test/java/batch21_group29/abc.java
@@ -0,0 +1,7 @@
+package batch21_group29;
+
+public class abc {
+    public static void main(String[] args) {
+
+    }
+}
